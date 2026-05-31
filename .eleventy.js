ca9e563de51bd7c2b96a8fc4f6dde7cc27a2ff67
cloudizzl <cloudizzl0@proton.me>
2026-05-31 12:28:31 +0200

    v1

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("dump", (obj) => JSON.stringify(obj));
+
+  // Keep .html extensions in output (Neocities compatibility)
+  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  // Pass through static assets unchanged
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("style.css");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
+    },
+    // Don't process node_modules or _site
+    pathPrefix: "/",
+  };
+};
